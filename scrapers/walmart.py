af9680830a82ef6afb8cd81dc026e61d8dfa191b
ilai.shohat <ilai.shohat@post.runi.ac.il>
2024-05-12 23:25:05 +0300

    Refactor Walmart scraper to use urllib.parse for URL manipulation and improve code readability

--- scrapers/walmart.py
+++ scrapers/walmart.py
@@ -1,55 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
-import random
-import time
+from urllib.parse import quote_plus, urlparse, parse_qs
+import re
 
-# List of user agents
-user_agents = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
-]
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+}
 
 def get_price_from_walmart(product_name):
-    product_name = product_name.replace(" ", "+")
-    url = f"https://www.walmart.com/search?q={product_name}"
-    
-    # Select a random user agent
-    user_agent = random.choice(user_agents)
-    headers = {"User-Agent": user_agent}
-    
-    # Add a random delay between requests
-    time.sleep(random.uniform(1, 3))
-    
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
-        
-        soup = BeautifulSoup(response.content, "html.parser")
-        
-        items = soup.select("div[data-item-id]")
-        
-        if not items:
+    url = 'https://www.walmart.com/search?q=' + quote_plus(product_name)
+    response = requests.get(url, headers=HEADERS)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        item_stack = soup.select('div[data-testid="item-stack"]')
+
+        if item_stack:
+            products = item_stack[0].find_all('div', class_='mb0 ph0-xl pt0-xl bb b--near-white w-25 pb3-m ph1')
+
+            if products:
+                for product in products:
+                    role_group_element = product.select_one('div[role="group"]')
+                    if role_group_element:
+                        sponsor = role_group_element.select_one('div[data-testid="list-view"] div div[class="mt5 mb0"] div')
+                        if not sponsor:
+                            product_url = role_group_element.find('a')
+                            if product_url:
+                                product_url = product_url.get('href')
+                                parsed_url = urlparse(product_url)
+                                query_params = parse_qs(parsed_url.query)
+                                final_url = query_params['rd'][0] if 'rd' in query_params else 'https://www.walmart.com' + product_url
+
+                                price_text = role_group_element.select_one('div[data-testid="list-view"] div[data-automation-id="product-price"] div').text.strip()
+                                if price_text:
+                                    numbers_and_dots = re.findall(r'[0-9.]+', price_text)
+                                    price = ''.join(numbers_and_dots)
+                                    price = price[:-2] + '.' + price[-2:]
+                                    price = float(price)
+                                    return final_url, price
+                                else:
+                                    return final_url, "Price not available"
+            else:
+                return "Product not found", "N/A"
+        else:
             return "Product not found", "N/A"
-        
-        for item in items:
-            item_name_element = item.select_one("span.lh-title")
-            if item_name_element:
-                item_name = item_name_element.text.strip()
-                price_element = item.select_one("div.lh-copy > span.lh-copy")
-                if price_element:
-                    price_text = price_element.text.strip().replace("$", "")
-                    try:
-                        price = float(price_text)
-                        return item_name, price
-                    except ValueError:
-                        # Handle the case when the price is not a valid float
-                        return item_name, "Price not available"
-        
-        return "Price not found", "N/A"
-    
-    except requests.exceptions.RequestException as e:
-        # Handle any exceptions that occur during the request
-        return "Error occurred while retrieving data from Walmart", str(e)
+    else:
+        raise Exception("Invalid Walmart search page response")
